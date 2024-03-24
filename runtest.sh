8d62eec1d9e01aa55deb3a284a7a7d5aee367c75
Max Wickham <maxwickham@startmail.com>
2024-03-23 23:29:11 -0500

    removed helper files

--- runtest.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-cd ../retesteth
-# sudo ./dretesteth.sh build
-# sudo ./dretesteth.sh -t GeneralStateTests/VMTests/vmArithmeticTest -- --singletest add \
-#    --testpath /home/max/Projects/ethereum_evm/tests/official_tests/tests --datadir /tests/config --filltests --vmtraceraw | more
